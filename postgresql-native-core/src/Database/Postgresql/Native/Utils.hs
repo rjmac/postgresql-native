09722ee8ff00ca4a6b5aa2f7571ed95f43948671
Robert J. Macomber <robertm@dilithium>
2014-10-07 16:17:20 -0700

    Put MD5Auth in its own module, reexported from Utils

--- postgresql-native-core/src/Database/Postgresql/Native/Utils.hs
+++ postgresql-native-core/src/Database/Postgresql/Native/Utils.hs
@@ -1,19 +1,5 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Database.Postgresql.Native.Utils (
   md5password
 ) where
 
-import qualified Data.ByteString as BS
-import Data.ByteString (ByteString)
-import Data.ByteString.Base16 (encode)
-import Data.Monoid
-import Data.Digest.MD5 (hash)
-
-import Database.Postgresql.Native.Types
-
-md5password :: ByteString0 -> ByteString0 -> ByteString -> ByteString0
-md5password username password salt =
-  let md5 = encode . BS.pack . hash . BS.unpack
-  in unsafeNewByteString0 ("md5" <> md5 (md5 (toByteString $ password <> username) <> salt))
-
+import Database.Postgresql.Native.Utils.MD5Auth (md5password)
