6da6c597e5763e0a32aa8e095ac20378306adf0b
Robert J. Macomber <robertm@dilithium>
2014-10-14 10:28:37 -0700

    Move simple Authenticators into the top-level module
    ...and leave Internal for machinery that Authenticators can use.

--- postgresql-native-core/src/Database/Postgresql/Native/Authenticator.hs
+++ postgresql-native-core/src/Database/Postgresql/Native/Authenticator.hs
@@ -4,4 +4,32 @@ module Database.Postgresql.Native.Authenticator (
 , passwordAuthenticator
 ) where
 
+import Database.Postgresql.Native.Types(AuthResultCode(..), ByteString0)
+import Database.Postgresql.Native.Message(FromFrontend(PasswordMessage))
+import Database.Postgresql.Native.Transport(sendMessage, sendSCMCredentials)
+import Database.Postgresql.Native.Utils(md5password)
 import Database.Postgresql.Native.Authenticator.Internal
+
+-- | An 'Authenticator' which can handle authentication methods that
+-- require no additional information (i.e., 'AuthOK' and
+-- 'SCMCredential')
+defaultAuthenticator :: Authenticator
+defaultAuthenticator = Authenticator auth
+    where auth AuthOK = Just $ \_ _ -> return ()
+          auth SCMCredential = Just $ \t _ -> do
+                                 sendSCMCredentials t
+                                 expectAuthOK t
+          auth _ = Nothing
+
+-- | An 'Authenticator' which can handle authentication methods that
+-- require a simple password (i.e., 'CleartextPassword' and
+-- 'MD5Password')
+passwordAuthenticator :: ByteString0 -> Authenticator
+passwordAuthenticator password = Authenticator auth
+    where auth CleartextPassword = Just $ \t _ -> do
+                                     sendMessage t $ PasswordMessage $ password
+                                     expectAuthOK t
+          auth (MD5Password salt) = Just $ \t username -> do
+                                      sendMessage t $ PasswordMessage $ md5password username password salt
+                                      expectAuthOK t
+          auth _ = Nothing
--- postgresql-native-core/src/Database/Postgresql/Native/Authenticator/Internal.hs
+++ postgresql-native-core/src/Database/Postgresql/Native/Authenticator/Internal.hs
@@ -3,17 +3,14 @@
 module Database.Postgresql.Native.Authenticator.Internal (
   Authenticator(..)
 , expectAuthOK
-, defaultAuthenticator
-, passwordAuthenticator
 ) where
 
 import Data.Monoid (Monoid, mempty, mappend)
 import Control.Applicative ((<|>))
 import Control.Exception (throwIO)
 import Database.Postgresql.Native.Types(AuthResultCode(..), ByteString0)
-import Database.Postgresql.Native.Message(FromBackend(..),FromFrontend(PasswordMessage))
-import Database.Postgresql.Native.Transport(Transport, nextMessage, sendMessage, sendSCMCredentials)
-import Database.Postgresql.Native.Utils(md5password)
+import Database.Postgresql.Native.Message(FromBackend(..))
+import Database.Postgresql.Native.Transport(Transport, nextMessage)
 import Database.Postgresql.Native.ProtocolError(ProtocolError(UnexpectedMessage))
 
 newtype Authenticator = Authenticator (AuthResultCode -> Maybe (Transport -> ByteString0 -> IO ()))
@@ -29,27 +26,3 @@ expectAuthOK t =
     AuthenticationResponse AuthOK -> return ()
     ErrorResponse oops -> error $ show oops -- TODO something better!
     other -> throwIO $ UnexpectedMessage other
-
--- | An 'Authenticator' which can handle authentication methods that
--- require no additional information (i.e., 'AuthOK' and
--- 'SCMCredential')
-defaultAuthenticator :: Authenticator
-defaultAuthenticator = Authenticator auth
-    where auth AuthOK = Just $ \_ _ -> return ()
-          auth SCMCredential = Just $ \t _ -> do
-                                 sendSCMCredentials t
-                                 expectAuthOK t
-          auth _ = Nothing
-
--- | An 'Authenticator' which can handle authentication methods that
--- require a simple password (i.e., 'CleartextPassword' and
--- 'MD5Password')
-passwordAuthenticator :: ByteString0 -> Authenticator
-passwordAuthenticator password = Authenticator auth
-    where auth CleartextPassword = Just $ \t _ -> do
-                                     sendMessage t $ PasswordMessage $ password
-                                     expectAuthOK t
-          auth (MD5Password salt) = Just $ \t username -> do
-                                      sendMessage t $ PasswordMessage $ md5password username password salt
-                                      expectAuthOK t
-          auth _ = Nothing
